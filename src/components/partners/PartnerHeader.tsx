d1bd49a438cfc74e73e6518fe9bfa5dabb593375
Lee.Pro <159094699+genaileepro@users.noreply.github.com>
2024-08-06 11:39:32 +0900

    docs: 충돌 병합 과정에 의한 수정 5차

--- src/components/partners/PartnerHeader.tsx
+++ src/components/partners/PartnerHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
-import icon from '../assets/icon.png'
+import icon from '../../assets/icon.png'
 
 const PartnerHeader: React.FC = () => {
 
